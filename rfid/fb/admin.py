9bda79401805b3e208f9fe7707d42fade0559fdb
Zhanybek Bekbolat <daveelfie@gmail.com>
2023-11-12 22:59:17 +0600

    Remove unnecessary models

--- rfid/fb/admin.py
+++ rfid/fb/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-from .models import Tag, Product
-# Register your models here.
-
-admin.site.register(Tag)
-admin.site.register(Product)
--- rfid/fb/urls.py
+++ rfid/fb/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('reg-tag', views.register_tag, name='tag-reg'),
-    path('reg-success', views.reg_success, name='success-page')
+    path('', views.index, name='index')
 ]
-
