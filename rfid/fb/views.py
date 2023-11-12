a4ad6b320baeb4cc0a66087741918fa23b740bb4
Zhanybek Bekbolat <daveelfie@gmail.com>
2023-11-12 12:56:43 +0600

    Connect to Firebase

---
+++ rfid/fb/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from decouple import config
+import pyrebase
+
+fb_config = {
+    'apiKey': config('API_KEY'),
+    'authDomain': config('AUTH_DOMAIN'),
+    'databaseURL': config('DATABASE_URL'),
+    'projectId': config('PROJECT_ID'),
+    'storageBucket': config('STORAGE_BUCKET'),
+    'messagingSenderId': config('MESSAGING_SENDER_ID'),
+    'appId': config('APP_ID'),
+}
+
+firebase = pyrebase.initialize_app(fb_config)
+authe = firebase.auth()
+database = firebase.database()
+
+
+# Create your views here.
+def index(request):
+    tag = database.child('tags').child('0x328ba84d').get().val()
+    return render(request, 'index.html', {
+        'tag': tag
+    })
--- rfid/rfid/settings.py
+++ rfid/rfid/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fb',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'rfid.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
